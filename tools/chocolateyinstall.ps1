a3308837125afdaec2df168305ecfe286c83fe09
Zhiming Wang <i@zhimingwang.org>
2019-06-23 11:41:33 +0800

    Upgrade to 0.9.4

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@
 
 $packageArgs = @{
   PackageName    = 'annie'
-  Url            = 'https://github.com/iawia002/annie/releases/download/0.9.3/annie_0.9.3_Windows_32-bit.zip'
-  Url64bit       = 'https://github.com/iawia002/annie/releases/download/0.9.3/annie_0.9.3_Windows_64-bit.zip'
-  Checksum       = 'e4b5a87f01b26d3c135105a39f70986cc2be6329676da651c871734e26f149b2'
+  Url            = 'https://github.com/iawia002/annie/releases/download/0.9.4/annie_0.9.4_Windows_32-bit.zip'
+  Url64bit       = 'https://github.com/iawia002/annie/releases/download/0.9.4/annie_0.9.4_Windows_64-bit.zip'
+  Checksum       = '99d73d061bbc128bdf44473405f1a94eefa1836b442d4766371fa6669a3e4519'
   ChecksumType   = 'sha256'
-  Checksum64     = '8a1feba060f488d714df32c65541b0fad1eb78a246962782fff05f150b773cf3'
+  Checksum64     = 'b9b71b70e74656db84acfdbee2ba8cab3a283e723097de57525394c2ab9099e1'
   ChecksumType64 = 'sha256'
   UnzipLocation  = "$(split-path -parent $MyInvocation.MyCommand.Definition)"
 }
