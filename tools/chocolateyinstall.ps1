e7d346a56a11b638f426f629b6948ac8c02fc622
Zhiming Wang <i@zhimingwang.org>
2020-05-29 14:09:00 +0000

    Update to v0.10.0 (from GitHub Actions)

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@
 
 $packageArgs = @{
   PackageName    = 'annie'
-  Url            = 'https://github.com/iawia002/annie/releases/download/0.9.4/annie_0.9.4_Windows_32-bit.zip'
-  Url64bit       = 'https://github.com/iawia002/annie/releases/download/0.9.4/annie_0.9.4_Windows_64-bit.zip'
-  Checksum       = '99d73d061bbc128bdf44473405f1a94eefa1836b442d4766371fa6669a3e4519'
+  Url            = 'https://github.com/iawia002/annie/releases/download/0.10.0/annie_0.10.0_Windows_32-bit.zip'
+  Url64bit       = 'https://github.com/iawia002/annie/releases/download/0.10.0/annie_0.10.0_Windows_64-bit.zip'
+  Checksum       = 'b65b4d2d6aa93d53e0787aae09f383423c09d6f6996d35ddd0e3965c7fd061f7'
   ChecksumType   = 'sha256'
-  Checksum64     = 'b9b71b70e74656db84acfdbee2ba8cab3a283e723097de57525394c2ab9099e1'
+  Checksum64     = 'd4f4bb5d22df0229b16ba9b48f771be60e2775263bee5c793c2f0865ce94ebb8'
   ChecksumType64 = 'sha256'
   UnzipLocation  = "$(split-path -parent $MyInvocation.MyCommand.Definition)"
 }
